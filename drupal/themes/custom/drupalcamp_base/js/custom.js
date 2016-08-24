4a2bde17286505246625dfea692a3832f791d665
Eirik S. Morland <eirik@morland.no>
2016-08-24 18:15:42 +0200

    Try to move to composer workflow.

--- drupal/themes/custom/drupalcamp_base/js/custom.js
+++
@@ -1,9 +0,0 @@
-;(function($) {
-  'use strict';
-  var windowHeight = $(window).height();
-  Drupal.behaviors.themeCustom = {
-    attach: function (context) {
-      $(context).find('#home').css('height', windowHeight);
-    }
-  }
-})(jQuery);
